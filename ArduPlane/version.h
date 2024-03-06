99548356d0949a8cb53d6a0db96c853c85ad8e79
Bob Long <robertlong13@gmail.com>
2024-03-06 08:41:18 -0500

    AgTS: prepare for 4.5.0.1-beta3 release

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.5.0-beta3"
+#define THISFIRMWARE "ArduPlane V4.5.0.1-beta3"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,0,FIRMWARE_VERSION_TYPE_BETA+2
